9c9240e641c02e4d0a49ee42cd6e5ddc0d89095d
Rafael Huijon <rhuijonp@gmail.com>
2018-02-22 17:06:18 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
+function getIt() {
+	$('p').on('click', function() {
+		alert('Hey');
+	})
+}
 
+function frameIt() {
+	$('img').addClass('tasty');
+}
+
+function pressIt() {
+	$('input#typing').on('keydown', function(key) { 
+		if (key.which === 71) {
+			alert("G");
+		}
+	})
+}
+
+function submitIt () {
+	$("form").on('submit', function(){
+		alert('Your form is going to be submitted now.');
+	});
+}
 $(document).ready(function(){
 
 // call functions here
-
+ $(document).on('load', getIt());
+	$(document).on('load', frameIt());
+	$(document).on('load', pressIt());
+	$(document).on('load', submitIt());
 });
