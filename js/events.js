2b9566668c37e86856a2f053e5c600b03d11e706
Rafael Huijon <rhuijonp@gmail.com>
2018-02-22 17:04:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,27 @@
 //define functions here
+function getIt() {
+	$('p').on('click', function() {
+		alert('Hey');
+	})
+}
 
+function frameIt() {
+	$('img').addClass('tasty');
+}
+
+function pressIt() {
+	$('input#typing').on('keydown', function(key) { 
+		if (key.which === 71) {
+			alert("G");
+		}
+	})
+}
+
+function submitIt () {
+	$("form").on('submit', function(){
+		alert('your form is going to be submitted now');
+	});
+}
 $(document).ready(function(){
 
 // call functions here
